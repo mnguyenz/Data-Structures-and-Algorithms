6d32369fccb1f47dd4ba474f18b6e49cc3e07818
Minh Nguyen <minh.nguyen@hodfords.com>
2023-09-19 11:25:36 +0700

    Some Leetcode

---
+++ algorithms/leetcode/128-longest-consecutive-sequence.ts
@@ -0,0 +1,37 @@
+// https://leetcode.com/problems/longest-consecutive-sequence/
+
+// Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+
+// You must write an algorithm that runs in O(n) time.
+
+// Example 1:
+
+// Input: nums = [100,4,200,1,3,2]
+// Output: 4
+// Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+// Example 2:
+
+// Input: nums = [0,3,7,2,5,8,4,6,0,1]
+// Output: 9
+
+// Constraints:
+
+// 0 <= nums.length <= 105
+// -109 <= nums[i] <= 109
+
+function longestConsecutive(nums: number[]): number {
+  const numSet: Set<number> = new Set(nums);
+  let result = 0;
+  for (const num of nums) {
+    if (!numSet.has(num - 1)) {
+      let length = 1;
+      while (numSet.has(num + length)) {
+        length++;
+      }
+      result = Math.max(result, length);
+    }
+  }
+  return result;
+};
+
+console.log(longestConsecutive([100, 4, 200, 1, 3, 2]));
