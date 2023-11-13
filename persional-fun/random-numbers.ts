bea72275440a6aff36af56905cd60f26da67c363
Minh Nguyen <minh.nguyen@hodfords.com>
2023-11-13 09:34:33 +0700

    Daily Leetcode and some other piece of code

---
+++ persional-fun/random-numbers.ts
@@ -0,0 +1,19 @@
+// Generate 10 random numbers from 1 - 120
+// Prepare to driver license test
+
+function randomNumbers(numberOfRandomNumbers: number, min: number, max: number): number[] {
+  const numbers: number[] = [];
+  while (numbers.length < numberOfRandomNumbers) {
+    const rn = getRandomNumber(min, max);
+    if (!numbers.includes(rn)) {
+      numbers.push(rn);
+    }
+  }
+  return numbers;
+};
+
+function getRandomNumber(min: number, max: number): number {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+console.log(randomNumbers(10, 1, 120));
